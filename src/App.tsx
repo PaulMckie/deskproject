51bd582d80f18554d1a9a38bd9f0739575434677
c-heighton <callum.h@me.com>
2022-10-11 17:01:06 +0100

    Create Initial Page with a Button
    Page loads with a new title, text and button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>DEMO Desk Booking System</p>
+      <button>I am a button click me!</button>
     </div>
   );
 }
