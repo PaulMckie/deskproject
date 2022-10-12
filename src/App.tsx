97d90a7e3cf5035b1e0c75897e8e80c4922e2d74
cherrelle <cherrelle_ward@hotmail.co.uk>
2022-10-12 14:48:15 +0100

    error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import './App.css';
-import { DeskRow } from './DeskRow';
-import { utils } from './utils';
-
+import React from "react";
+import "./App.css";
+import { DeskRow } from "./DeskRow.tsx";
+import { utils } from "./utils.tsx";
 
 //utils - generates 5 rows
 export function App(): any {
-  const userID = 'User';
+  const userID = "User";
 
   return (
     <div className="App">
       <p>DEMO Desk Booking System</p>
-      {utils.range(0, 4).map(rowNumber => (
-        < DeskRow
-          key={rowNumber}
-          row={rowNumber}
-        />
+      {utils.range(0, 4).map((rowNumber) => (
+        <DeskRow key={rowNumber} row={rowNumber} />
       ))}
     </div>
   );
