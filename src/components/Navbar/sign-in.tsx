4f5ebd2939432e483b76f0c58a7faafc478d822f
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-10-13 09:08:07 +0100

    Navbar

---
+++ src/components/Navbar/sign-in.tsx
@@ -0,0 +1,11 @@
+import "../Navbar/nav.css";
+import React from "react";
+const Modal = () => {
+  return (
+    <div className="signModal">
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Modal;
