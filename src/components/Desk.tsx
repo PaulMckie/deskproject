bf3974f72e5dd10e9d7480e69ddfda29e54baef7
Paul.Mckie <paul.mckie@raytheon.co.uk>
2022-10-13 16:14:46 +0100

    working project

--- src/components/Desk.tsx
+++ src/components/Desk.tsx
@@ -5,26 +5,50 @@ import "../CSS/Desk.css";
 
 interface MyProps {
   deskNum: IDeskInfo;
-  userID: IDeskInfo;
+  bookingUserID: IDeskInfo;
+  bookingDate: IDeskInfo;
 }
 
-const Desk: FC<MyProps> = ({ deskNum, userID }) => {
+const Desk: FC<MyProps> = ({ deskNum, bookingUserID, bookingDate }) => {
   const [self, setSelf] = useState(new deskInfoClass(deskNum));
-
   const [selected, setSelected] = useState(false);
 
-  const getModalText = (): string => {
+  const getModalText = (bookingUserID: string, bookingDate: Date): string => {
     let text: string;
 
-    if (self.getBookedStatus() && userID === self.userID) {
+    const status: number = self.isDateBooked(bookingUserID, bookingDate);
+
+    if (status === 2) {
       text = `Desk ${self.getDeskID()} is already booked by yourself. Do you want to unbook it?`;
-    } else if (self.getBookedStatus()) {
+    } else if (status === 1) {
       text = `Desk ${self.getDeskID()} is already booked by ${self.userID}.`;
     } else {
       text = `You have selected desk ${self.getDeskID()} to book. Is this correct?`;
     }
 
     return text;
+  };
+
+  const getButtonColour = (checkDate: Date): string => {
+
+    const status = self.isDateBooked(bookingUserID, checkDate);
+
+    console.log(`Updating button colour`, self.getBookedStatus());
+
+    switch (status) {
+      case 0:
+        return 'green'
+      
+      case 1:
+        return 'red'
+      
+      case 2:
+        return 'purple'
+      
+      default:
+        return 'black'
+    }
+
   };
 
   return (
@@ -36,11 +60,7 @@ const Desk: FC<MyProps> = ({ deskNum, userID }) => {
           setSelected(true);
         }}
         style={{
-          backgroundColor: !self.getBookedStatus()
-            ? "green"
-            : userID === self.userID
-            ? "purple"
-            : "red",
+          backgroundColor: getButtonColour(bookingDate)
         }}
       >
         {self.getDeskID()}
@@ -51,15 +71,15 @@ const Desk: FC<MyProps> = ({ deskNum, userID }) => {
       >
         <div className="BookDeskInterface">
           <span>
-            <p>{getModalText()}</p>
+            <p>{getModalText(bookingUserID, bookingDate)}</p>
             <button
               onClick={() => {
-                self.toggleDeskBook(userID);
+                self.toggleDeskBook(bookingUserID, bookingDate);
                 setSelected(false);
               }}
               style={{
                 display:
-                  self.getBookedStatus() && userID !== self.userID
+                  self.isDateBooked(bookingUserID, bookingDate) === 1
                     ? "none"
                     : "inherit",
               }}
